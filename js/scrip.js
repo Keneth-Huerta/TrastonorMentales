b48e3b38abbd299527f34ae7b93ab03e0c9d61cc
Keneth Huerta <kenethissac@gmail.com>
2023-12-17 02:07:31 -0600

    algo

--- js/scrip.js
+++ js/scrip.js
@@ -5,7 +5,7 @@ function accion() {
         ancla[i].classList.toggle("desaparece");
     }
 }
-function ani(){
-    var anima=document.getElementById("qr");
+function ani() {
+    var anima = document.getElementById("qr");
     anima.classList.toggle("qr")
 }
